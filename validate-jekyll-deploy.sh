a07bdee08451bf8b27dc58fc528f9f7f0ef528df
Paul Rosenthal <Paul.H.Rosenthal@iCloud.com>
2024-04-06 11:59:31 -0500

    Add Line to echo Ruby Version in validate-jekyll-deploy.sh

--- validate-jekyll-deploy.sh
+++ validate-jekyll-deploy.sh
@@ -7,6 +7,7 @@ sudo bundle install
 # The $2 variable contains the location of the Jekyll website. By default, this is the root of the repository
 # and is equal to /github/workspace.
 cd /github/workspace/$2 && echo "Changed directory to `pwd`"
+echo "The current Ruby version is: " && ruby -v
 if jekyll build --verbose ; then
   echo "The test build completed successfully."
   exit 0
